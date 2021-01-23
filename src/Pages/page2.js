a4fe2d8fddb9787bacfdb5c0bfef2ac22c91d516
Ankita Bantey <38222723+ankitabantey@users.noreply.github.com>
2021-01-23 16:16:48 -0600

    toggle menu

--- src/Pages/page2.js
+++ src/Pages/page2.js
@@ -2,17 +2,22 @@ import './page2.css';
 import React from "react";
 import { Button } from 'react-bootstrap';
 import history from './history';
+import ReactDOM from 'react-dom';
+import Demo from './components/demo';
 /* Page 2 */
 
 
+
 function page2() {
+    ReactDOM.render(<Demo />, document.querySelector('#root'));
     return ( 
         <div className = "chat">
             <h5>Today's Covid Risk</h5>
+
             <z>73%<br></br></z>
-            <Button variant="btn btn-success" onClick={() => {history.push('/page3'); window.location.reload();}}>Click for page3</Button>
+           
              {/* Dropdown menu */}
-             <label for="cars">How many people have you seen in the last 2 weeks?</label>
+            <label for="cars">How many people have you seen in the last 2 weeks?</label>
 
             <select name="cars" id="cars">
             <option value="audi"></option>
@@ -21,6 +26,10 @@ function page2() {
             <option value="mercedes">3</option>
             <option value="mercedes">4</option>
             </select>
+           
+            <p></p>
+            <Button variant="btn btn-success" onClick={() => {history.push('/page3'); window.location.reload();}}>Click for page3</Button>
+            
         </div>
     );
 }
